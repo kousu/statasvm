3ef5996d439a7bb031e02520da2ca6d0ea6b5695
nguenthe <nguenthe@uwaterloo.ca>
2015-05-13 20:48:10 -0400

    Link against libsvm
    Doing this POSIX-style on OS X requires trickery.
    If there is a way to *tell* the linker to do POSIX-style filename-only dependencies, I want to hear it.
    
    Untested on Windows or Linux, yet.

--- src/Darwin.mk
+++ src/Darwin.mk
@@ -8,8 +8,14 @@ CFLAGS+=-DSYSTEM=APPLEMAC	#the Stata Programming Interface (i.e. stplugin.h) nam
 LDFLAGS+=-bundle
 
 
+# the complicated install_name_tool line takes everything in LIBS and rewrites them with as a filename; this means that LD_LIBRARY_PATH and DYLD_LIBRARY_PATH will be searched at runtime for this library. This goes against the OS X conventions, but the OS X conventions don't understand package management.
+# this *doesn't* use @rpath, but it could, and it might if I reneg on this opinion and switch to bundling.
+# TODO: if we decide to bundle libsvm.dylib, we'll also need to add the current directory to where the .dylib will look for depends, like Windows. On Linux, people often write wrappers that manipulate LD_LIBRARY_PATH before launch, but OS X lets us bundle this information *into the executable*: use `-Wl,-rpath,.` (or maybe `-Wl,-rpath,@executable_path`). See `man ld`
+
 %.dylib:
-	$(CC) $(CFLAGS) $(LDFLAGS) $^ -o $@ #or should this be -dynamiclib? it gives a different filetype, but still works
+	$(CC) $(CFLAGS) $(LDFLAGS) $(foreach L,$(LIBS),-l$L) $^ -o $@ #or should this be -dynamiclib? it gives a different filetype, but still works
+	$(foreach L,$(LIBS),ABS=$$(otool -L $@ | tail -n +2 | grep $L | cut -f 1 -d " ") && install_name_tool -change $$ABS $$(basename $$ABS) $@ &&) true
+
 
 
 # --- testing ---

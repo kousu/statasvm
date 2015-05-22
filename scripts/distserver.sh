ac075ac1c6dfb01bddbea847e03d31940c4f2273
nguenthe <nguenthe@uwaterloo.ca>
2015-05-22 17:00:16 -0400

    Naming clarity.
    dist.sh doesn't make the dist, it hosts it.

---
+++ scripts/distserver.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+HERE=$(dirname $0)
+cd $HERE/../src/dist &&
+echo Putting the package repo online &&
+echo Contents: &&
+ls -l --color=auto &&
+echo use ". net from http://localhost:8000" to test it out &&
+# try several common basic HTTP servers which are probably on-system
+# (this 
+(python -m SimpleHTTPServer || python -m http.server || webfsd -d . ) 2>/dev/null 
+

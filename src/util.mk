8864827bc790d01c80603b1f16ef640aafd29cdf
nguenthe <nguenthe@uwaterloo.ca>
2015-05-21 22:08:22 -0400

    Experimental recursish include make macro
    This may or may not come in handy.

--- src/util.mk
+++ src/util.mk
@@ -17,3 +17,49 @@ FixPath = $1
 # Make does not offer a recursive wildcard function, so here's one:
 # (from LightStruk @ http://stackoverflow.com/questions/3774568/makefile-issue-smart-way-to-scan-directory-tree-for-c-files)
 rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
+
+
+# credits to [GMSL](http://gmsl.sourceforge.net/) for this routine
+define chop
+$(wordlist 2,$(words $(1)),x $(1))
+endef
+
+
+# Extended include routine which helps building non-recursive makefiles
+# you have to write all your file paths with $(PWD)/ prefixed,
+# and you need to run it as $(eval $(call cludine,<path/to/submakefile1> <path/to/submakefile2>))
+# but these are the only nuisances
+#
+#
+# this works by, as suggested at http://evbergen.home.xs4all.nl/nonrecursive-make.html,
+#  keeping the current directory available in a variable (which you cannot do with 'pwd')
+# --> if make just had something like __FILE__ this wouldn't be a problem. argh.
+# however this approach is simpler: you don't need any boilerplate in the submakefiles.
+#
+# 
+#
+# notice: all the evals are necessary to achieve anything within a macro, see
+# http://stackoverflow.com/questions/5751099/defining-variables-within-a-makefile-macro-define
+# because macros were primarily designed as text hacking abilities
+ifndef PWD #set up a default PWD, if the system hasn't already set it
+  ifeq ($(OS),Windows_NT)
+    PWD:=$(shell cd) #from http://www.lemoda.net/windows/windows2unix/windows2unix.html
+  else
+    PWD:=$(shell pwd)
+  endif
+endif
+define _cludine
+# push
+$(eval _PWD:=$(_PWD) $(PWD))
+$(eval PWD:=$(dir $(1)))
+# call
+$(eval include $(1))
+# pop
+$(eval PWD:=$(lastword $(_PWD)))
+$(eval _PWD:=$(call chop,$(_PWD)))
+endef
+define cludine
+$(foreach i,$(1),$(eval $(call _cludine,$(i))))
+endef
+
+$(eval $(call cludine,tst/Makefile tst/tst/Makefile))

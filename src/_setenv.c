4ab610d31f3c8e56a73e33ad21c9df49afbf2736
nguenthe <nguenthe@uwaterloo.ca>
2015-05-27 20:08:04 -0400

    And remove debug prints

--- src/_setenv.c
+++ src/_setenv.c
@@ -27,9 +27,6 @@ STDLL stata_call(int argc, char *argv[])
 {
     ST_retcode err = 0;
     
-    
-    stdisplay("_setenv: first %s = %s\n", argv[0], getenv(argv[0]));
-    
     if(argc == 1) {
       err = unsetenv(argv[0]);
       if(err) {
@@ -46,8 +43,6 @@ STDLL stata_call(int argc, char *argv[])
         sterror("_setenv: incorrect number of arguments (%d), can only 1 or 2\n", argc);
         return 1;
     }
-    
-    stdisplay("_setenv: now %s = %s\n", argv[0], getenv(argv[0]));
     
     return 0;
 }

6486b249e573b94c2ed17018b2b45723e81196f7
nguenthe <nguenthe@uwaterloo.ca>
2015-06-03 15:09:55 -0400

    Bugfix: return empty on no-var, instead of not returning
    The difference is in this:
    plugin call _getenv, PATH
    plugin call _getenv, NONEXISTENT
    -> `_getenv' would contain the value for PATH, instead of ""
       with this change it contains ""

--- src/_getenv.c
+++ src/_getenv.c
@@ -7,21 +7,26 @@
 
 #define OUTPUT "__getenv" //two underscores means that it is a local macro (the first underscore) with name "_getenv" (to match the plugin)
 
+const char* EMPTY = "";
+
 STDLL stata_call(int argc, char *argv[])
 {
     ST_retcode err = 0;
     
+    // argument checking
     if(argc != 1) {
         sterror("_getenv: incorrect number of arguments (%d), can only take 1\n", argc);
         return 1;
     }
     
+    // lookup the environemnt variable
     char* value = getenv(argv[0]);
     if(value == NULL) {
         //sterror("_getenv: '%s' not found.\n", argv[0]); //DEBUG
-        return 0;
+        value = EMPTY;
     }
     
+    // return (via a Stata macro, because the plugin interface offers nothing else)
     err = SF_macro_save(OUTPUT, value);
     if(err) {
       sterror("_getenv: unable to write '%s' to output macro '%s'[%d]: \n", err, value, OUTPUT, strerror(err), err);

345be039d5287ae00bb3db283301bc1c405ff653
Nick <nguenthe@uwaterloo.ca>
2015-11-18 11:26:55 -0500

    Comment and clean the stata wrappers

--- scripts/stata.bat
+++ scripts/stata.bat
@@ -1,34 +1,45 @@
-		rem Wrap Stata so that scripts (do-files) return error codes up to the OS like other languages
-rem run like
-rem  `stata.bat script.do & echo %errorlevel%`
-rem
-rem This is a port of stata.sh, which should be sibling to it.
+REM Wrap Stata so that scripts (do-files) `behave like a Unix interpreter:
+REM - the only argument is a .do file to run
+REM - the output is sent to stdout
+REM  - But because of how Stata is hard-coded you won't see output until the entire script is finished
+REM - exit codes are bubbled up to the OS (though Stata exit codes are 16 bit at least and C exit codes are only 8)
+REM 
+REM run like
+REM  `stata.bat script.do & echo %errorlevel%`
+REM
+REM This is a port of stata(.sh), which you should find in the same directory, and shares a dependency on stata_wrap.do `with `that other version.
 
 echo off
 
-rem TODO: read the Stata install location from the registry ?
+REM Make sure Stata is findable
 set PATH=C:\Program Files (x86)\Stata14;C:\Program Files\Stata14;C:\Program Files (x86)\Stata13;C:\Program Files\Stata13;C:\Program Files (x86)\Stata12;C:\Program Files\Stata12;%PATH%
-
-rem Make sure Stata is findable
-rem TODO: handle falling back to stata-32 as well
 set STATA=stata-64
 where "%STATA%" 2>NUL
 if ERRORLEVEL 1 echo Stata not found & exit /b 1
 
 set SCRIPT=%1%
 
-
+REM http://weblogs.asp.net/whaggard/get-directory-path-of-an-executing-batch-file
 set WRAPPER=%~dp0\stata_wrap.do
 
-rem Windows doesn't have mktemp, so we cop out and hop this works
+REM Windows doesn't have mktemp, so we cop out and hope this works
 set CRUFT=_statawrapped
-mkdir %CRUFT%
+mkdir %CRUFT% 2>NUL
 set LOG=%CRUFT%\wrapped.log
 set RC=%CRUFT%\rc.txt
 
+
 "%STATA%" -q -e do "%WRAPPER%" "%SCRIPT%" "%LOG%" "%RC%"
 
+
+
 type %LOG%
+
+REM http://stackoverflow.com/questions/3068929/how-to-read-file-contents-into-a-variable-in-a-batch-file
 set /p RC=<%RC%
-rem rmdir /s /q %CRUFT%
+
+REM http://stackoverflow.com/questions/97875/rm-rf-equivalent-for-windows
+rmdir /s /q %CRUFT%
+
+REM /b means "only the .bat"; without it exit takes down the entire terminal!
 exit /b %RC%

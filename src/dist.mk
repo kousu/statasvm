b6d27f6c0fd22cf1be74ce2e2c212a29d16c95dd
Nick Guenther <nguenthe@uwaterloo.ca>
2016-03-07 17:25:15 -0600

    In `make collect`, make sure the target dir exist first.

--- src/dist.mk
+++ src/dist.mk
@@ -68,6 +68,7 @@ endif
 collect: bin/$(PLATFORM)
 	@echo 'make collect' is meant to be run VM->Mac Mini build host.
 	@echo It collects the platform-specific parts into one place.
+	-ssh 10.0.2.2 mkdir -p statasvm/src/bin/
 	scp -r bin/$(PLATFORM) 10.0.2.2:statasvm/src/bin/
 	
 # quick hack: deploy to my personal account
